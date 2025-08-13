34900070c1d782f27e2ccc31fe5a4ddf6087f494
thanhnx3 <ngothanh201100@gmail.com>
2025-08-13 18:39:36 +0700

    Fix lỗi CORS backend

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use(express.json())
 // app.use(cors())
 app.use(cors({
   origin: ["https://ticktock-seven.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
